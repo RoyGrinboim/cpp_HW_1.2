9e14759bb0970b5823e543043efe52c8eed71905
RoyGrinboim <57876891+RoyGrinboim@users.noreply.github.com>
2020-04-02 14:34:44 +0300

    Add files via upload

--- PhoneticFinder.cpp
+++ PhoneticFinder.cpp
@@ -76,7 +76,7 @@ namespace phonetic{
             }
         }       
         string s = "Did not find the word '" + word + "' in the text"; 
-        //throw(s);
+        throw(s);
         //cerr<<s<<endl;
         return s;
         
